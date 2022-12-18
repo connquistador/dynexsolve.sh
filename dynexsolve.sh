2ff1c71823ac0a2caaef0874ec010f8af14f6d37
connquistador <97854426+connquistador@users.noreply.github.com>
2022-12-18 13:58:28 -0600

    Create dynexsolve.sh

---
+++ dynexsolve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /DynexSolve/
+
+./dynexsolve -mining-address XwmfP47uuu4XY3YmGcaaLoibEWyLVVgjrGV1agd6j3iGHCjZLoSSBGLTDgnM4X3qjsZrgfgojbnGEZRVepMAQYce28bcV5Txn@KNQR1 -no-cpu -multi-gpu -stratum-url minenice.newpool.pw -stratum-port 1183 -stratum-password KNQRDYNEX -daemon-host dnx.us.ekapool.com -daemon-port 19666
